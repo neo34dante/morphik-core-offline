b419e7202e40e41592aafdfb8de9af80803514a1
Neo34dante <onli4rah@gmail.com>
2025-05-27 03:50:30 +0530

    Updated

--- setup.sh
+++ setup.sh
@@ -14,12 +14,14 @@ sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
+sudo apt install curl ca-certificates gnupg
 sudo apt install -y python3.12 python3.12-venv python3.12-dev
-
-# 2. Install PostgreSQL 14
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /usr/share/keyrings/postgresql.gpg
+echo "deb [signed-by=/usr/share/keyrings/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+# 2. Install PostgreSQL 17
 echo_msg "Installing PostgreSQL 17"
 sudo apt update
-sudo apt install -y postgresql postgresql-contrib
+sudo apt install -y postgresql-17 postgresql-contrib-17
 
 # 3. Install pgvector
 echo_msg "Installing pgvector extension"
